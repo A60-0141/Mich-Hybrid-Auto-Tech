2a793da8e5b179b995a38c5b11948b4ea70a85c4
A60-0141 <endedenaville@gmail.com>
2025-02-11 21:39:37 +0300

    migrations

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f^u@bwr%wspr!&3btezd-3b=^jh80sqbtag1%!s@$fe62@d7tf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mich-hybrid-auto-tech-production.up.railway.app']
 
